553ec00c4c99ff5ac49afbb83d16569309c3ce97
TheDarkStrix <thedarkstrix@gmail.com>
2023-05-24 13:41:19 +0530

    update noob-trader

--- noob-trader/_next/static/jls0fLXYzDysyYUpI0Uvl/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002Freset\u002F[reset-id]","\u002Fverify\u002F[verification-id]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
